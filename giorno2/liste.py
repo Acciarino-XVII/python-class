7853373a0b2e589b27f9b4d1d3c94ce8a9f9d914
Acciarino-XVII <acciarokarim@gmail.com>
2025-02-18 23:38:11 +0100

    liste completate

---
+++ giorno2/liste.py
@@ -0,0 +1,35 @@
+print("esercizio 1")
+a=[]
+print("--------------------------------")
+
+print("esercizio 2")
+a=[1,2,3,4,5]
+
+print("--------------------------------")
+
+print("esercizio 3")
+print(a[2])
+
+print("--------------------------------")
+
+print("esercizio 4")
+a.append(6)
+print(a[5])
+
+print("--------------------------------")
+
+print("esercizio 5")
+a.pop(3)
+print("--------------------------------")
+
+print("esercizio 6")
+b = a[:3]
+print(b)
+print("--------------------------------")
+
+print("esercizio 7")
+print("la lista senza sort: ", a)
+a.sort(reverse=True,)
+
+print("la lista con sort: ", a)
+print("--------------------------------")
